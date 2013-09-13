d6f8c4d1c000b36d507848ca7df712e517365c9e
Valeriy Provalov <valeriyp@microsoft.com>
2013-09-13 15:36:49 -0700

    registration mode added

--- unregister.cmd
+++ unregister.cmd
@@ -1,3 +1,6 @@
 @echo off
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /unregister bin\x64\Release\PassIE.dll
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister bin\x86\Release\PassIE.dll
+SET PASSIEDLL=Release\PassIE.dll
+IF NOT [%1]==[] SET PASSIEDLL=%1\PassIE.dll
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /unregister bin\x64\%PASSIEDLL%
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister bin\x86\%PASSIEDLL%
